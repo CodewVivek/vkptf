42af38e158a941fd46a81af22a67d7e7d9cfa082
CodewVivek <vivekmanikonda113@gmail.com>
2025-08-23 18:53:39 +0530

    Update hero.jsx

--- components/hero.jsx
+++ components/hero.jsx
@@ -55,7 +55,7 @@ export default function Hero() {
                         </Link>
 
                         <Link
-                            href="https://www.linkedin.com/in/haquedot/"
+                            href=" https://www.linkedin.com/in/sai-vivek-manikonda-10b734249?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app  "
                             target="_blank"
                         >
                             <button className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-9 rounded-md px-3 border border-input bg-background hover:bg-accent hover:text-accent-foreground">
